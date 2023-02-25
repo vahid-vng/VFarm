05fef7b2bb0e0eb0b317c06dba9c4157d90e0238
Vahid Najafi <106664722+vahid-vng@users.noreply.github.com>
2023-02-25 12:42:05 +0330

    Add files via upload

---
+++ scripts/slider.js
@@ -0,0 +1,17 @@
+$(".owl-carousel").owlCarousel({
+  rtl: true,
+  loop: true,
+  margin: 10,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    // 600: {
+    //   items: 3,
+    // },
+    // 1000: {
+    //   items: 1,
+    // },
+  },
+});
